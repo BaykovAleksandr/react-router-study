7c5fe0a3d6bda7598306cf0866492557129938e4
Baykov Aleksandr <baykov-1988@mail.ru>
2025-07-24 21:31:23 +0300

    add pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Categories from './pages/Categories';
+import Cart from './pages/Cart';
+import NotFound from './pages/NotFound';
+import ProductDetails from './pages/ProductDetails';
+
+const router = createBrowserRouter([
+  { path: "/", element: <Home /> },
+  { path: "about", element: <About /> },
+  { path: "cart", element: <Cart /> },
+  { path: "categories", element: <Categories /> },
+  { path: "home", element: <Home /> },
+  { path: "product", element: <ProductDetails /> },
+  { path: "*", element: <NotFound /> },
+]);
+
 function App() {
-  return <div></div>;
+  return <RouterProvider router={router}/>;
 }
 
 export default App;
